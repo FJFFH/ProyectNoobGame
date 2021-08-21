c34f1e8ecf41f823e111951c14af351ae9f85342
FJFFH <59668794+FJFFH@users.noreply.github.com>
2021-08-20 20:44:05 -0500

    movimiento y rotación

--- Assets/Fran/Script/Personaje.cs
+++ Assets/Fran/Script/Personaje.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Personaje : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float horizontalMove;
+    public float verticalMove;
+    public CharacterController player;
+    public float playerSpeed;
+    
     void Start()
     {
-        
+        player = GetComponent<CharacterController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        player.Move(new Vector3(horizontalMove, 0, verticalMove) * playerSpeed * Time.deltaTime);
     }
 }
